c131f8a4d16c2c95b8a658a492b29354e6284a4d
Cédric GOUJON <cedgoujon78@gmail.com>
2023-08-30 22:30:21 +0200

    try

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    serverRuntimeConfig: {
+      databaseUrl: process.env.DATABASE_URL || 'mongodb://localhost/taskmanager',
+      baseUrl: process.env.BASE_URL || 'http://localhost:3000',
+    },
+    publicRuntimeConfig: {},
+  };
+  
